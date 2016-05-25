8fa66c733ace24851e0d703f735a8e2cb15e079a
abc <abc@KAROL>
2016-05-25 12:49:23 +0200

    DONE Signed-off-by:abc <abc@KAROL>

--- src/pretius/test/PretiusTest.java
+++ src/pretius/test/PretiusTest.java
@@ -5,6 +5,10 @@
  */
 package pretius.test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author abc
@@ -15,8 +19,31 @@ public class PretiusTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        double amount = 0;
         
-        
+        try {
+            FileReader in = new FileReader(args[0]);
+            BufferedReader buffReader = new BufferedReader(in);
+            String line = null;
+
+            while ((line = buffReader.readLine()) != null) {
+                String[] words = line.split(":");
+                words[words.length - 1] = words[words.length - 1].replace(",", ".");
+                if (words[words.length - 1].isEmpty() == false) {
+                    StringBuffer buffer = new StringBuffer(words[words.length - 1]);
+                    buffer.delete(buffer.length() - 3, buffer.length());
+                    double price = Double.parseDouble(buffer.substring(0));
+                    price = Math.round(price);
+                    amount += price;
+                }
+            }
+
+            System.out.println("Suma kwot zapisanych w pliku: " + amount + "PLN");
+            in.close();
+        } catch (IOException e) {
+            System.out.println("Problem: " + e);
+        }
     }
-    
+
 }
